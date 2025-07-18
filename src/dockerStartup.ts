c2c847fa582476e95281b2ceac0ea0d750d04a37
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2025-07-18 12:25:17 +0100

    change to messages.json

--- src/dockerStartup.ts
+++ src/dockerStartup.ts
@@ -2,7 +2,7 @@ import { NTFYWebhookListener } from "./listeners/NTFYWebhookListener";
 import { Server } from "./rss/Server";
 import { JSONMessageWriter } from "./writers/JSONPropertyWriter";
 
-const ntfy = new NTFYWebhookListener(new JSONMessageWriter("/app/thing.json"));
+const ntfy = new NTFYWebhookListener(new JSONMessageWriter("/app/messages.json"));
 const server = new Server(3000, "/app");
 ntfy.listen();
 server.listen();
--- src/localStartup.ts
+++ src/localStartup.ts
@@ -3,7 +3,7 @@ import { Server } from "./rss/Server";
 import { JSONMessageWriter } from "./writers/JSONPropertyWriter";
 require("dotenv").config();
 
-const ntfy = new NTFYWebhookListener(new JSONMessageWriter("./thing.json"));
+const ntfy = new NTFYWebhookListener(new JSONMessageWriter("./messages.json"));
 const server = new Server(3000, "./");
 ntfy.listen();
 server.listen();
